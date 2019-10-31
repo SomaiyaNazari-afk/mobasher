f85163e26a8bd72fa18973b12c8b593ffd321562
Somaia Nazary <somaia.nazary@netlinks.af>
2019-10-31 08:40:55 +0430

    Intial commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(20),
+            'email' => Str::random(20).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
