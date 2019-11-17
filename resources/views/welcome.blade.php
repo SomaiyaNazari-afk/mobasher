d47157acf6d8d47807cc7fd8e00560e2c9aea3fd
Maryam Naderi <maryamnaderi1227@gmail.com>
2019-11-17 16:16:49 +0430

    Changes to welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,8 @@
                     Pharmacy
                 </div>
 
+
+				<h1>Hello ALl</h1>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
